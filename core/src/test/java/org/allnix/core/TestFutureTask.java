0c1d85ce13e56f24f2e3e36a7ad86bced3171756
Yi-Kun Yang <ykyang@gmail.com>
2016-11-09 19:41:03 -0600

    Test cancel external process

--- core/src/test/java/org/allnix/core/TestFutureTask.java
+++ core/src/test/java/org/allnix/core/TestFutureTask.java
@@ -15,11 +15,13 @@
  */
 package org.allnix.core;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -35,6 +37,7 @@ public class TestFutureTask {
   static private final Logger logger = LoggerFactory.getLogger(
     TestFutureTask.class);
   private ExecutorService executor;
+
   @BeforeClass(alwaysRun = true)
   public void beforeClass() {
     executor = Executors.newCachedThreadPool();
@@ -71,13 +74,51 @@ public class TestFutureTask {
         return "done";
       }
     );
-    
+
     executor.execute(task);
-    
+
     boolean success = task.cancel(true);
 
     Assert.assertTrue(success);
     Assert.assertTrue(task.isCancelled());
     Assert.assertTrue(task.isDone());
+  }
+
+  @Test(groups = {"short"})
+  public void testCancelExternalProcess() {
+
+    // > Start an external program
+    // > wait indefinitely on the external program
+    FutureTask<String> task = new FutureTask<String>(
+      () -> {
+        ProcessBuilder pb = new ProcessBuilder("xterm");
+        Process process = null;
+        try {
+          process = pb.start();
+          process.waitFor();
+        } catch (IOException ex) {
+          throw new RuntimeException(ex);
+        } catch (InterruptedException ex) {
+          process.destroyForcibly();
+        }
+        return "12345";
+      }
+    );
+
+    executor.execute(task);
+
+    // > Expect timeout
+    Assert.assertThrows(TimeoutException.class, () -> {
+      task.get(10, TimeUnit.MICROSECONDS);
+    });
+
+    // Uncomment this to see the xterm pops up
+//    try {
+//      TimeUnit.SECONDS.sleep(1);
+//    } catch (InterruptedException ex) {
+//    }
+//
+    // > Cancel --interrupts--> task thread --> waitFor() --> destroyForcibly()
+    task.cancel(true);
   }
 }
