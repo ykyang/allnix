a7bf1355f17a3a04861d03cc99bcbc19eaa1b92a
Yi-Kun Yang <ykyang@gmail.com>
2016-11-13 18:50:00 -0600

    More functional stuff

--- core/src/test/java/org/allnix/core/TestFunctionalOperation.java
+++ core/src/test/java/org/allnix/core/TestFunctionalOperation.java
@@ -15,11 +15,16 @@
  */
 package org.allnix.core;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
@@ -28,9 +33,25 @@ import org.testng.annotations.Test;
  */
 public class TestFunctionalOperation {
 
+  static private final Logger logger = LoggerFactory.getLogger(
+    TestFunctionalOperation.class);
+
+  private List<Integer> intList;
+  private int intMin, intMax;
+
+  @BeforeTest(alwaysRun = true)
+  void beforeTest() {
+    intMin = 0;
+    intMax = 1000;
+    intList = new ArrayList<>();
+    for (int i = intMax; i >= intMin; i--) {
+      intList.add(i);
+    }
+  }
+
   @Test
   public void test() {
-    List<Integer> list = Arrays.asList(7, 6, 5, 4, 3, 2, 1);
+    List<Integer> list = intList; //Arrays.asList(7, 6, 5, 4, 3, 2, 1);
     list = list.stream()
       .filter((x) -> {
         out.println("Filtering: " + x);
@@ -38,13 +59,59 @@ public class TestFunctionalOperation {
       })
       .map((x) -> {
         out.println("Mapping: " + x);
-        return x*x;
+        return x * x;
       })
       .limit(2)
       .collect(Collectors.toList());
-    
+
     list.forEach((i) -> {
       out.println(i);
     });
+  }
+
+  @Test
+  public void testAllMatch() {
+    List<Integer> list = intList;
+    boolean ans = list.stream()
+      .allMatch((x) -> {
+        return x < 500;
+      });
+
+    Assert.assertFalse(ans);
+
+    ans = list.stream().allMatch((x) -> {
+      return x <= 1000;
+    });
+
+    Assert.assertTrue(ans);
+  }
+
+  @Test
+  public void testFinaAny() {
+    List<Integer> list = intList;
+
+    Optional<Integer> ans = list.stream().findAny();
+
+    Assert.assertTrue(intMin <= ans.get());
+    Assert.assertTrue(ans.get() <= intMax);
+
+    ans.ifPresent(out::println);
+  }
+
+  @Test
+  public void testReduce() {
+    List<Integer> list = intList;
+
+    int ans = list.stream()
+      .reduce(0, (a, b) -> {
+        return a + b;
+      });
+    
+    Assert.assertEquals(ans, (intMin+intMax)*(intMax-intMin+1)/2);
+    
+    ans = list.stream()
+      .reduce(Integer.MIN_VALUE, Integer::max);
+    
+    Assert.assertEquals(ans, intMax);
   }
 }
