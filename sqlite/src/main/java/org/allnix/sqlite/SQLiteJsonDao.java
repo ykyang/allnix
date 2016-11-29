bc9aac8b9c0a9e14cf005c3afea48392db3d7ad4
Yi-Kun Yang <ykyang@gmail.com>
2016-11-28 19:50:51 -0600

    Add SQLite JSON DAO and tests

---
+++ sqlite/src/main/java/org/allnix/sqlite/SQLiteJsonDao.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2016 .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.sqlite;
+
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.sqlite.SQLiteDataSource;
+import org.sqlite.SQLiteJDBCLoader;
+
+/**
+ *
+ * @author Yi-Kun Yang &gt;ykyang@gmail.com&lt;
+ */
+public class SQLiteJsonDao {
+
+  private JdbcTemplate jdbcTemplate;
+
+  public void init() throws Exception {
+    boolean success = SQLiteJDBCLoader.initialize();
+    SQLiteDataSource dataSource = new SQLiteDataSource();
+    dataSource.setUrl("jdbc:sqlite:job.db");
+
+    jdbcTemplate = new JdbcTemplate(dataSource);
+  }
+
+  public void createTable(String tableName) {
+    String sql
+      = "CREATE TABLE IF NOT EXISTS %s (\n"
+      + "  Id    CHAR(36)        PRIMARY KEY,\n"
+      + "  Json  TEXT            NOT NULL\n"
+      + ");";
+
+    sql = String.format(sql, tableName);
+
+    jdbcTemplate.execute(sql);
+  }
+
+  public boolean create(String tableName, String id, String json) {
+    final String insert = "INSERT OR IGNORE INTO %s VALUES (?,?);";
+    
+    String sql = String.format(insert, tableName);
+    int rowAffected = jdbcTemplate.update(sql, id, json);
+
+    return rowAffected == 1;
+  }
+
+  public boolean delete(String tableName, String id) {
+    final String delete = "DELETE FROM %s where Id = ?";
+    
+    String sql = String.format(delete, tableName);
+    int rowAffected = jdbcTemplate.update(sql, id);
+    
+    return rowAffected == 1;
+  }
+  
+  public String read(String tableName, String id) {
+    final String read = "SELECT Json FROM %s WHERE Id = ?";
+    
+    String sql = String.format(read, tableName);
+    
+    try {
+      String json = jdbcTemplate.queryForObject(sql, String.class, id);
+      return json;
+    } catch(IncorrectResultSizeDataAccessException ex) {
+      return null;
+    }
+  }
+  
+  public boolean update(String tableName, String id, String json) {
+    final String update = "UPDATE %s SET Json = ? WHERE Id = ?";
+    String sql = String.format(update, tableName);
+    
+    int rowCount = jdbcTemplate.update(sql, json, id);
+    
+    return rowCount == 1;
+  }
+}
---
+++ sqlite/src/test/java/org/allnix/sqlite/TestSQLiteJsonDao.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.sqlite;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Yi-Kun Yang &gt;ykyang@gmail.com&lt;
+ */
+public class TestSQLiteJsonDao {
+
+  private SQLiteJsonDao dao;
+  static private final String JOB_INPUT = "JobInput";
+
+  @BeforeTest(alwaysRun = true)
+  void beforeTest() throws Exception {
+    dao = new SQLiteJsonDao();
+    dao.init();
+
+    dao.createTable(JOB_INPUT);
+  }
+
+  @Test
+  public void test() {
+    String id = "1";
+    String json = "{}";
+    boolean result;
+    String text;
+
+    result = dao.create(JOB_INPUT, id, json);
+    Assert.assertTrue(result);
+
+    // > Second create should fail
+    result = dao.create(JOB_INPUT, id, json);
+    Assert.assertFalse(result);
+
+    text = dao.read(JOB_INPUT, id);
+    Assert.assertEquals(text, json);
+
+    json = "{\"method\":\"run\"}";
+
+    result = dao.update(JOB_INPUT, id, json);
+    Assert.assertTrue(result);
+
+    text = dao.read(JOB_INPUT, id);
+    Assert.assertEquals(text, json);
+
+    result = dao.delete(JOB_INPUT, id);
+    Assert.assertTrue(result);
+
+    // > Read returns null
+    text = dao.read(JOB_INPUT, id);
+    Assert.assertNull(text);
+    
+    // > Second delete should fail
+    result = dao.delete(JOB_INPUT, id);
+    Assert.assertFalse(result);
+  }
+}
