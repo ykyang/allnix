be2b8a9c2247766eaf1013ddee8f5052984dd39b
Yi-Kun Yang <ykyang@gmail.com>
2016-12-16 15:55:08 -0600

    My first Rest service
    My first Rest service that serves JSON with given table name
    and ID.

---
+++ rest-json/src/main/java/org/allnix/restjson/Application.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Yi-Kun Yang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.restjson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ *
+ * @author Yi-Kun Yang &gt;ykyang@gmail.com&lt;
+ */
+@Configuration
+@EnableAutoConfiguration
+@Import(value = {org.allnix.restjson.web.Config.class})
+@ComponentScan
+public class Application {
+  static private final Logger logger = LoggerFactory.getLogger(Application.class);
+  public static void main(String[] args) {
+    logger.info("START");
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ rest-json/src/main/java/org/allnix/restjson/web/JsonController.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Yi-Kun Yang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.restjson.web;
+
+import org.allnix.core.JsonDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Yi-Kun Yang &gt;ykyang@gmail.com&lt;
+ */
+@RestController
+@RequestMapping(value="/json")
+public class JsonController {
+  static private Logger logger = LoggerFactory.getLogger(JsonController.class);
+  
+  private JsonDao dao;
+
+  public JsonController() {
+    
+  }
+  
+  @Autowired
+  public void setJsonDao(JsonDao dao) {
+    logger.info("Set JsonDao");
+    this.dao = dao;
+  }
+
+  
+  @GetMapping(value="/{table}/{id}")
+  public String read(@PathVariable String table, @PathVariable String id) {
+    return table + ": " + id;
+//     return dao.read(table, id);
+  }
+}
