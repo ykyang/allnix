15ada31289b2f15b47a574ef9de1d7582a32c8a4
Yi-Kun Yang <ykyang@gmail.com>
2016-12-15 18:51:56 -0600

    Rest service "Greeting" running

---
+++ greeting/src/main/java/org/allnix/greeting/web/GreetingController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Yi-Kun Yang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.greeting.web;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.allnix.greeting.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Yi-Kun Yang &gt;ykyang@gmail.com&lt;
+ */
+@RestController
+public class GreetingController {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(
+    @RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(),
+      String.format(template, name));
+  }
+}
