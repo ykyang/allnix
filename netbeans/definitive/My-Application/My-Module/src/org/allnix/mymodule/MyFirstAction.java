f6b048b295602a7b745570b6d38c30ecd4151332
Yi-Kun Yang <ykyang@gmail.com>
2017-07-11 15:23:43 -0500

    LEARN: Write to Output Window from Action

--- netbeans/definitive/My-Application/My-Module/src/org/allnix/mymodule/MyFirstAction.java
+++ netbeans/definitive/My-Application/My-Module/src/org/allnix/mymodule/MyFirstAction.java
@@ -7,8 +7,11 @@ package org.allnix.mymodule;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import org.netbeans.api.io.IOProvider;
+import org.netbeans.api.io.InputOutput;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
 
@@ -20,12 +23,25 @@ import org.openide.util.NbBundle.Messages;
   iconBase = "org/allnix/mymodule/ic_directions_bike_black.png",
   displayName = "#CTL_MyFirstAction"
 )
-@ActionReference(path = "Menu/Edit", position = 1200)
+@ActionReferences({
+  @ActionReference(path = "Menu/Edit", position = 1200),
+  @ActionReference(path = "Toolbars/MyToolbars", position = 100)
+})
 @Messages("CTL_MyFirstAction=My First Action")
 public final class MyFirstAction implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
     // TODO implement action body
+    InputOutput io = IOProvider.getDefault().getIO("Task", true);
+    
+    // > Open the Output Window
+    io.show();
+    
+    io.getOut().println("Standand output");
+    io.getErr().println("Error output");
+    
+    io.getOut().close();
+    io.getErr().close();
   }
 }
