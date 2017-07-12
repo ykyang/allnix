3f352d929f7e18afe08478ffad309b0c60358509
Yi-Kun Yang <ykyang@gmail.com>
2017-07-12 15:33:55 -0500

    Intermediate: Ch3 p. 68

---
+++ netbeans/developer/TaskManager/TaskIdGenerator/src/com/netbeansrcp/taskidgenerator/TaskIdGeneratorImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 Yi-Kun Yang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netbeansrcp.taskidgenerator;
+
+import com.netbeansrcp.taskidgenerator.api.TaskIdGenerator;
+import java.util.Random;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Yi-Kun Yang &lt;ykyang at gmail.com&gt;
+ */
+@ServiceProvider(service=TaskIdGenerator.class)
+public class TaskIdGeneratorImpl implements TaskIdGenerator {
+  private Random random = new Random();
+  public String generateId() {
+    String id = "000000" + random.nextInt();
+    id = id.substring(id.length() - 6);
+    return id;
+  }
+  
+}
