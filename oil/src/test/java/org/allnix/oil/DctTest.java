3e2c43c400bb49b756cf46bae971d96ba731becf
Yi-Kun Yang <ykyang@gmail.com>
2018-05-15 16:58:01 -0500

    Initialize oil

---
+++ oil/src/test/java/org/allnix/oil/DctTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Yi-Kun Yang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.allnix.oil;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig(classes = {  })
+@TestInstance(Lifecycle.PER_CLASS)
+public class DctTest {
+
+	
+}
---
+++ oil/src/test/resources/assumption.py
@@ -0,0 +1,4 @@
+import sys
+a = 43
+b = 34.0
+c = a + b
