7442ce8f31baa2c63f037081ce200acb7640c22a
Yi-Kun Yang <ykyang@gmail.com>
2018-06-17 16:12:58 -0500

    oil: test optismistic locking with version

--- oil/src/test/java/org/allnix/oil/lab/JUnit5LabServiceTest.java
+++ oil/src/test/java/org/allnix/oil/lab/JUnit5LabServiceTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,8 @@ package org.allnix.oil.lab;
 
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.OptimisticLockException;
 
 import org.allnix.oil.TestSpringApplication;
 import org.allnix.oil.lab.model.Core;
@@ -34,6 +36,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -45,7 +48,7 @@ import static org.junit.jupiter.api.Assertions.*;
 //@SpringBootApplication(exclude = { MongoAutoConfiguration.class,
 //    MongoDataAutoConfiguration.class })
 @SpringBootTest(classes = TestSpringApplication.class,
-webEnvironment = WebEnvironment.NONE)
+    webEnvironment = WebEnvironment.NONE)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { TestConfig.class })
 @TestInstance(Lifecycle.PER_CLASS)
@@ -56,12 +59,12 @@ public class JUnit5LabServiceTest {
     private DefaultLabService ls;
     @Autowired
     private ProjectLoader pl;
-    
+
     @BeforeAll
     public void beforaAll() {
         pl.create();
     }
-    
+
     @Commit
     @Test
     @Tag("seconds")
@@ -69,37 +72,58 @@ public class JUnit5LabServiceTest {
         Well well;
         Optional<Well> wellOpt;
         List<Core> coreList;
-        
+
         wellOpt = //
             ps.findFirstWellByName(ProjectLoader.ROSE_CHILDREN);
         Assertions.assertNotNull(wellOpt.orElse(null));
         well = wellOpt.get();
-        
+
         //< core >//
         coreList = ls.findCoreByWell(well);
         Assertions.assertEquals(10, coreList.size());
-        
+
         //< CT >//
         for (Core core : coreList) {
             Optional<Ct> ctOpt = ls.findCtByCore(core);
             Assertions.assertNotNull(ctOpt.get());
         }
     }
-    
+
     @Commit
     @Test
     @Tag("milliseconds")
     public void testVersion() {
         Core core, core0;
-        
+
         core = new Core();
         assertEquals(0, core.version());
+
         core.setName("Core-1");
-        
-        
         core0 = ls.save(core);
-        
-        
-        
+
+        assertEquals(0, core.version());
+        assertEquals(0, core0.version());
+
+        core0.setTopCoreDepth(1000.);
+        Core core1 = ls.save(core0);
+
+        assertEquals(0, core.version());
+        assertEquals(0, core0.version());
+        assertEquals(1, core1.version());
+
+        //> Test Optimistic lock
+        //> Try to save an older version
+        assertThrows(ObjectOptimisticLockingFailureException.class, //
+            () -> {
+                // Save version = 0 should fail
+                ls.save(core0);
+            });
+        core0.setTopCoreDepth(1001.);
+        assertThrows(ObjectOptimisticLockingFailureException.class, //
+            () -> {
+                // Save version = 0 should fail
+                ls.save(core0);
+            });
+
     }
 }
