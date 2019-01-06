09e777a0af7bb64237b6535a79ff1151564005e7
Yi-Kun Yang <ykyang@gmail.com>
2019-01-06 16:06:19 -0600

    vtk: add polyline

---
+++ vtk/src/main/java/org/allnix/vtk/PolyLine.java
@@ -0,0 +1,76 @@
+package org.allnix.vtk;
+
+import vtk.*;
+
+public class PolyLine {
+	static 
+	  {
+	    if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+	    {
+	      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+	      {
+	        if (!lib.IsLoaded()) 
+	        {
+	          System.out.println(lib.GetLibraryName() + " not loaded");
+	        }
+	      }
+	    }
+	    vtkNativeLibrary.DisableOutputWindow(null);
+	  }
+	public static void main(String[] args) {
+		vtkNamedColors colors = new vtkNamedColors();
+
+		vtkPoints points = new vtkPoints();
+		
+		double[] origin = {0, 0, 0};
+		double[] p0 = {1, 0, 0};
+		double[] p1 = {0, 1, 0};
+		double[] p2 = {0, 1, 2};
+		double[] p3 = {1, 2, 3}; 
+		points.InsertNextPoint(origin);
+		points.InsertNextPoint(p0);
+		points.InsertNextPoint(p1);
+		points.InsertNextPoint(p2);
+		points.InsertNextPoint(p3);
+		
+		vtkPolyLine polyLine = new vtkPolyLine();
+		polyLine.GetPointIds().SetNumberOfIds(5);
+		for (int ind = 1; ind < 5; ind++) {
+			polyLine.GetPointIds().SetId(ind, ind);
+		}
+	
+		vtkCellArray cells = new vtkCellArray();
+		cells.InsertNextCell(polyLine);
+		
+		vtkPolyData polyData = new vtkPolyData();
+		polyData.SetPoints(points);
+		
+		polyData.SetLines(cells);
+		
+		vtkPolyDataMapper mapper = new vtkPolyDataMapper();
+		mapper.SetInputData(polyData);
+		
+		vtkActor actor = new vtkActor();
+		actor.SetMapper(mapper);
+		double[] color = new double[4];
+		colors.GetColor("Tomato", color);
+		actor.GetProperty().SetColor(color);
+
+		vtkRenderer renderer = new vtkRenderer();
+		vtkRenderWindow renWin = new vtkRenderWindow();
+		renWin.SetWindowName("Poly Line");
+		renWin.AddRenderer(renderer);
+		vtkRenderWindowInteractor renWinInt = new vtkRenderWindowInteractor();
+		renWinInt.SetRenderWindow(renWin);
+		renderer.AddActor(actor);
+		
+		color = new double[4];
+		colors.GetColor("DarkOliveGreen", color);
+		renderer.SetBackground(color);
+		
+		renWin.Render();
+		renWinInt.Start();
+	    renWinInt.Initialize();
+		
+	}
+}
