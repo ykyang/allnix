506ccd99428fedfceb973eddc1ac75ae6da6ce41
Yi-Kun Yang <ykyang@gmail.com>
2019-01-19 16:43:36 -0600

    vtk: add Junit5 example

---
+++ vtk/src/test/java/org/allnix/gui/ExampleTest.java
@@ -0,0 +1,29 @@
+package org.allnix.gui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ExampleTest {
+	static final private Logger logger = LoggerFactory.getLogger(ExampleTest.class);
+	
+	@BeforeAll
+	public void beforeClass() {
+		logger.info("@BeoforeAll");
+	}
+	
+	@Test
+	@Tag("second")
+	public void testEqual() {
+		int expected = 1;
+		int actual = 1;
+		assertEquals(expected, actual);
+	}
+}
