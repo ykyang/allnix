3e7fffa30a8cd99a9100cbc1aab6ea099127cac5
Yi-Kun Yang <ykyang@gmail.com>
2019-01-21 09:59:05 -0600

    vtk: use VtkThreshold in VtkGeomodel

--- vtk/src/main/java/org/allnix/gui/DefaultVtkService.java
+++ vtk/src/main/java/org/allnix/gui/DefaultVtkService.java
@@ -1,8 +1,25 @@
 package org.allnix.gui;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class DefaultVtkService {
-	private Map<String, VtkUnstructuredGrid> vugridDb;
-	private Map<String, VtkThreshold> ijkThresholdDb;
+	private Map<String, VtkUnstructuredGrid> ugridDb;
+	/**
+	 * ID -> VtkTrheshold mapping
+	 */
+	private Map<String, VtkThreshold> thresholdDb;
+	
+	public DefaultVtkService() {
+		ugridDb = new HashMap<>();
+		thresholdDb = new HashMap<>();
+	}
+	
+	
+	public void putVtkThreshold(String id, VtkThreshold v) {
+		thresholdDb.put(id, v);
+	}
+	public VtkThreshold getVtkThreshold(String id) {
+		return thresholdDb.get(id);
+	}
 }
--- vtk/src/main/java/org/allnix/gui/VtkGeomodel.java
+++ vtk/src/main/java/org/allnix/gui/VtkGeomodel.java
@@ -1,6 +1,5 @@
 package org.allnix.gui;
 
-import java.rmi.UnexpectedException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -32,8 +31,14 @@ public class VtkGeomodel {
 	 * Object[0]: VtkUnstructuredGrid
 	 * Object[1]: vtkThreshold
 	 */
-	private Map<String, Object[]> ijkDb;
+//	private Map<String, Object[]> ijkDb;
+	private DefaultVtkService vsvc;
+	/**
+	 * Map ijk slice name to ID in VtkService
+	 */
+//	private Map<String, String> ijkMap;
 	private VtkRenderer renderer;
+	
 //	private VtkFrame frame;
 	
 //	public void setVtkFrame(VtkFrame frame) {
@@ -47,12 +52,13 @@ public class VtkGeomodel {
 		mainGrid = new VtkUnstructuredGrid();
 		mainGrid.init();
 		
-		ijkDb = LazyMap.lazyMap(new HashMap<String, Object[]>(), new Factory<Object[]>() {
-			@Override
-			public Object[] create() {
-				return new Object[3];
-			}
-		});
+		vsvc = new DefaultVtkService();
+//		ijkDb = LazyMap.lazyMap(new HashMap<String, Object[]>(), new Factory<Object[]>() {
+//			@Override
+//			public Object[] create() {
+//				return new Object[3];
+//			}
+//		});
 	}
 	
 	/**
@@ -73,60 +79,73 @@ public class VtkGeomodel {
 		// TODO: Check existing name
 		
 		// TODO: Check i,j,k
-		vtkThreshold threshold = new vtkThreshold();
-		threshold.SetInputData(mainGrid.getUnstructuredGrid());
-		threshold.ThresholdBetween(1, 1);
-		threshold.SetInputArrayToProcess(0, 0, 0, 
-				1, // vtkDataObject.FIELD_ASSOCIATION_CELLS
-				dir + "_index" //field name
-				//"i_index" // field name
-				);
-		threshold.Update();
+		VtkThreshold vtk = new VtkThreshold();
+		vtk.init();
+		vtk.setInputData(mainGrid.getUnstructuredGrid());
+		vtk.setThresholdBetween(1, 1);
+		vtk.setCellInputArrayToProcess(dir + "_index");
+		vtk.update();
 		
-//		vtkUnstructuredGrid grid = threshold.GetOutput();
-//		
-		VtkUnstructuredGrid augrid = new VtkUnstructuredGrid();
-		augrid.setUnstructuredGrid(threshold.GetOutput());
-////		wgrid.setLookupTable(mainGrid.getLookupTable());
-		augrid.init();
+//		vtkThreshold threshold = new vtkThreshold();
+//		threshold.SetInputData(mainGrid.getUnstructuredGrid());
+//		threshold.ThresholdBetween(1, 1);
+//		threshold.SetInputArrayToProcess(0, 0, 0, 
+//				1, // vtkDataObject.FIELD_ASSOCIATION_CELLS
+//				dir + "_index" //field name
+//				//"i_index" // field name
+//				);
+//		threshold.Update();
 		
-		Object[] objz = this.ijkDb.get(name);
-		objz[0] = augrid;
-		objz[1] = threshold;
+//		VtkUnstructuredGrid augrid = new VtkUnstructuredGrid();
+//		augrid.setUnstructuredGrid(threshold.GetOutput());
+//		augrid.init();
+		
+		
+		this.vsvc.putVtkThreshold(name, vtk);
+//		Object[] objz = this.ijkDb.get(name);
+//		objz[0] = augrid;
+//		objz[1] = threshold;
 	}
 	public void setIJKSliceActiveScalar(String sliceName, String name) {
-		Object[] objz = this.ijkDb.get(sliceName);
-		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0];
+		VtkThreshold threshold = vsvc.getVtkThreshold(sliceName);
+		
+//		Object[] objz = this.ijkDb.get(sliceName);
+		VtkUnstructuredGrid vugrid = threshold.getOutput();
 		vugrid.setActiveScalars(name);
 	}
 	public void showIJKSlice(String name) {
-		Object[] objz = this.ijkDb.get(name);
-		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0];
+		VtkThreshold threshold = vsvc.getVtkThreshold(name);
+		
+//		Object[] objz = this.ijkDb.get(name);
+		VtkUnstructuredGrid vugrid = threshold.getOutput();
 		renderer.addActor(vugrid.getActor());
 	}
 	public void hideIJKSlice(String name) {
-		Object[] objz = this.ijkDb.get(name);
-		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0];
+		VtkThreshold threshold = vsvc.getVtkThreshold(name);
+		
+//		Object[] objz = this.ijkDb.get(name);
+		VtkUnstructuredGrid vugrid = threshold.getOutput();
 		renderer.removeActor(vugrid.getActor());
 	}
 	public void setIJKSliceThresholdColorRange(String name, double[] minmax) {
-		Object[] objz = this.ijkDb.get(name);
-		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0]; // this is stupid
+		VtkThreshold threshold = vsvc.getVtkThreshold(name);
+		
+//		Object[] objz = this.ijkDb.get(name);
+		VtkUnstructuredGrid vugrid = threshold.getOutput();		
+		
 		vugrid.setLookupTableRange(minmax);
 	}
 	
 	public void setIJKSliceThresholdBetween(String name, double min, double max) {
-		Object[] objz = this.ijkDb.get(name);
-		
-//		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0];
-//		if (vugrid != null) {
-//		  vtkUnstructuredGrid oldGrid = vugrid.getUnstructuredGrid();
-//		}
-		vtkThreshold threshold = (vtkThreshold) objz[1]; // stupid
+		// Object[] objz = this.ijkDb.get(name);
+		VtkThreshold threshold = vsvc.getVtkThreshold(name);
+		threshold.setThresholdBetween(min, max);
+		threshold.update();
+//		vtkThreshold threshold = (vtkThreshold) objz[1]; // stupid
 		
 		
-		threshold.ThresholdBetween(min, max);
-		threshold.Update();
+//		threshold.ThresholdBetween(min, max);
+//		threshold.Update();
 		// > after the Update(), threshold returns the same grid object as before
 		// vtkUnstructuredGrid newGrid = threshold.GetOutput();
 		
@@ -135,54 +154,10 @@ public class VtkGeomodel {
 		
 		// remove old actor
 		
-		VtkUnstructuredGrid vugrid = (VtkUnstructuredGrid) objz[0];
+		VtkUnstructuredGrid vugrid = threshold.getOutput();
 		
-//		if (vugrid == null) {
-//			vugrid = new VtkUnstructuredGrid();
-//			vugrid.setUnstructuredGrid(threshold.GetOutput());
-//			vugrid.init();
-//			vugrid.setActiveScalars("Temperature"); // TODO: where do we get this from
-//			
-//			objz[0] = vugrid;
-//			
-//			frame.addActor(vugrid.getActor());
-//		} else {
-//			if (vugrid.getUnstructuredGrid() != threshold.GetOutput()) {
-//				throw new RuntimeException("The two vtkUnstructuredGrid are expected to be the same");
-//			}
-//		}
 		// > reset active scalar name
 		vugrid.setActiveScalars(vugrid.getActiveScalarName());
-//		vugrid.getMapper().SetInputData(vugrid.getUnstructuredGrid());
-//		vugrid.getMapper().Update();
-		
-//		vugrid.getUnstructuredGrid().GetCellData().Modified();
-//		vugrid.getUnstructuredGrid().Modified();
-//		vugrid.getActor().Modified();
-		
-//		vugrid.getMapper().StaticOff();
-//		vugrid.getMapper().SetInputData(vugrid.getUnstructuredGrid());
-		
-//		if (vugrid != null) {
-//			frame.removeActor(vugrid.getActor());
-//			vtkUnstructuredGrid oldGrid = vugrid.getUnstructuredGrid();
-//			vtkUnstructuredGrid newGrid = threshold.GetOutput();
-//			logger.info("oldGrid: {}", oldGrid);
-//			logger.info("newGrid: {}", newGrid);
-//		}
-				
-//		vugrid = new VtkUnstructuredGrid();
-//		vugrid.setUnstructuredGrid(threshold.GetOutput());
-//		vugrid.init();
-//		vugrid.setActiveScalars("Temperature"); // TODO: where do we get this from
-//		
-//		
-//		
-//		objz[0] = vugrid;
-////		objz[1] = threshold;
-//		
-//		frame.addActor(vugrid.getActor());
-//		// TODO: add new actor
 	}
 	
 	static public void main(String[] args) throws InterruptedException {
