d53a421e0cfa0380bb36fa4ac32dc9ae2dfd2833
Yi-Kun Yang <ykyang@gmail.com>
2019-12-22 20:10:25 -0600

    Find first free port in RMI

--- core/src/main/java/org/allnix/rmi/TemperatureServerImpl.java
+++ core/src/main/java/org/allnix/rmi/TemperatureServerImpl.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * ./gradlew -PmainClass=org.allnix.rmi.TemperatureServerImpl runApp
  * 
@@ -16,44 +19,60 @@ import java.util.Random;
  *
  */
 public class TemperatureServerImpl implements TemperatureServer, Runnable {
+	static private Logger logger = LoggerFactory.getLogger(TemperatureServerImpl.class);
 
 	private List<TemperatureListener> listenerList = new ArrayList<>();
 	private volatile double temperature;
-	
+
 	protected TemperatureServerImpl() {
 		temperature = 88;
 	}
-	
+
 	public static void main(String[] args) throws RemoteException {
 		TemperatureServerImpl server = new TemperatureServerImpl();
 		Remote stub = UnicastRemoteObject.exportObject(server, 0);
-		Registry registry = LocateRegistry.createRegistry(1099);
-		registry.rebind("TemperatureService", stub);
-		Thread thread = new Thread(server);
-		thread.start();
-		System.out.println("Temperature Service Started");
+		boolean done = false;
+		int port = 1099;
+		while (!done && port < 65535) {
+			try {
+				Registry registry = LocateRegistry.createRegistry(port);
+				registry.rebind("TemperatureService", stub);
+				Thread thread = new Thread(server);
+				thread.start();
+				System.out.println("Temperature Service Started");
+				done = true;
+				logger.info("Use port: {}", port);
+			} catch (java.rmi.server.ExportException e) {
+				logger.info("Port in use: {}", port);
+				port++;
+			}
+		}
+		if (!done) {
+			logger.error("Cannot find a free port");
+		}
+
 	}
-	
+
 	@Override
 	public void run() {
 		Random random = new Random();
-		while(true) {
+		while (true) {
 			int duration = random.nextInt() % 10000 + 2000;
 			duration = Math.abs(duration);
-		
+
 			try {
 				Thread.sleep(duration);
 			} catch (InterruptedException e) {
 				// ignore
 			}
-			
+
 			int dir = random.nextInt();
 			if (dir < 0) {
 				temperature -= 0.5;
 			} else {
 				temperature += 0.5;
 			}
-			
+
 			this.notifyTemperatureListeners(temperature);
 		}
 	}
@@ -67,7 +86,7 @@ public class TemperatureServerImpl implements TemperatureServer, Runnable {
 			}
 		}
 	}
-	
+
 	@Override
 	public void addTemperatureListener(TemperatureListener listener) {
 		listenerList.add(listener);
