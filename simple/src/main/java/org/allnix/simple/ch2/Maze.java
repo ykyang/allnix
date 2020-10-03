f48f4142b38907d4767c8847a6c35a83f1cc5f95
Yi-Kun Yang <ykyang@gmail.com>
2020-10-03 09:41:14 -0500

    CSP: 2.2.2

--- simple/src/main/java/org/allnix/simple/ch2/Maze.java
+++ simple/src/main/java/org/allnix/simple/ch2/Maze.java
@@ -1,6 +1,8 @@
 package org.allnix.simple.ch2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Maze {
 	/**
@@ -39,12 +41,17 @@ public class Maze {
 	private final MazeLocation start, goal;
 	private Cell[][] grid;
 	
+	public Maze() {
+		this(10,10,new MazeLocation(0, 0), new MazeLocation(9,9), 0.2);
+	}
+	
 	public Maze(int rowCount, int columnCount,
 			MazeLocation start, MazeLocation goal,
 			double sparseness) {
 		super();
 		this.rowCount = rowCount;
 		this.columnCount = columnCount;
+		// TODO: Check for null?
 		this.start = start;
 		this.goal = goal;
 		
@@ -60,8 +67,41 @@ public class Maze {
 		grid[goal.row][goal.column] = Cell.GOAL;
 	}
 	
-	public Maze() {
-		this(10,10,new MazeLocation(0, 0), new MazeLocation(9,9), 0.2);
+	public List<MazeLocation> successors(MazeLocation ml) {
+		List<MazeLocation> locations = new ArrayList<>();
+		
+		int r,c; // row and column to be tested
+		boolean blocked;
+		
+		r = ml.row + 1;
+		c = ml.column;
+		blocked = grid[r][c] == Cell.BLOCKED;
+		if (r < rowCount && !blocked) {
+			locations.add(new MazeLocation(r, c));
+		}
+		
+		r = ml.row - 1;
+		c = ml.column;
+		blocked = grid[r][c] == Cell.BLOCKED;
+		if (-1 < r && !blocked) {
+			locations.add(new MazeLocation(r, c));
+		}
+		
+		r = ml.row;
+		c = ml.column + 1;
+		blocked = grid[r][c] == Cell.BLOCKED;
+		if (c < columnCount && !blocked) {
+			locations.add(new MazeLocation(r, c));
+		}
+		
+		r = ml.row;
+		c = ml.column - 1;
+		blocked = grid[r][c] == Cell.BLOCKED;
+		if (-1 < c && !blocked) {
+			locations.add(new MazeLocation(r, c));
+		}
+		
+		return locations;
 	}
 	
 	public String toString() {
@@ -84,6 +124,11 @@ public class Maze {
 				}
 			}
 		}
+	}
+	
+	private boolean goalTest(MazeLocation ml) {
+		// goal could be null
+		return goal.equals(ml);
 	}
 	
 }
