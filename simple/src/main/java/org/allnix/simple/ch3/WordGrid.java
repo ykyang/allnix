cd83e74fbb95af9e7cca65d81faa5c4493511eb8
Yi-Kun Yang <ykyang@gmail.com>
2020-10-17 20:13:31 -0500

    CSP: 3.4

--- simple/src/main/java/org/allnix/simple/ch3/WordGrid.java
+++ simple/src/main/java/org/allnix/simple/ch3/WordGrid.java
@@ -1,5 +1,7 @@
 package org.allnix.simple.ch3;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -10,6 +12,20 @@ import java.util.Random;
  *
  */
 public class WordGrid {
+    /**
+     * ./gradlew -PmainClass=org.allnix.simple.ch3.WordGrid runApp
+     * gradlew.bat -PmainClass=org.allnix.simple.ch3.WordGrid runApp
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        
+        GridLocation[] l = new GridLocation[10];
+        l[0] = new GridLocation(1,2);
+        out.println(l);
+    }
+    
     private final int ALPHABET_LENGTH = 26;
     private final char FIRST_LETTER = 'A';
     private final int rows, columns;
@@ -32,13 +48,13 @@ public class WordGrid {
         }
     }
     
-    public void mark(String word, List<GridLocation> locations) {
+    public void mark(String word, GridLocation[] locations) {
         for (int i = 0; i < word.length(); i++) {
-            GridLocation loc = locations.get(i);
+            GridLocation loc = locations[i];
             grid[loc.row][loc.column] = word.charAt(i);
         }
     }
-    
+        
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (char[] row : grid) {
@@ -49,4 +65,81 @@ public class WordGrid {
         return sb.toString();
     }
     
+    public List<GridLocation[]> generateDomain(String word) {
+       List<GridLocation[]> domain = new ArrayList<>();
+       int length = word.length();
+       
+       // Get all the possible locations
+       for (int row = 0; row < rows; row++) {
+           for (int col = 0; col < columns; col++) {
+               if (col + length <= columns) {
+                   domain.add(fill90(row,col,length));
+                   if (row + length <= rows) {
+                       domain.add(fill135(row,col,length));
+                   }
+               }
+               if (row + length <= rows) {
+                   domain.add(fill180(row,col,length));
+                   if (col - length >= 0) {
+                       domain.add(fill225(row,col,length));
+                   }
+               }
+           }
+       }
+       
+       return domain;
+    }
+    /**
+     * Fill word to the right (90 degree)
+     * 
+     * @param row
+     * @param column
+     * @param length
+     * @return
+     */
+    private GridLocation[] fill90(int row, int column, int length) {
+        GridLocation[] gls = new GridLocation[length];
+                
+        for (int c = column, ind = 0; c < (column+length); c++, ind++) {
+            gls[ind] = new GridLocation(row,c);
+        }
+        
+        return gls;
+    }
+    
+    private GridLocation[] fill135(int row, int column, int length) {
+        GridLocation[] gls = new GridLocation[length];
+        
+        for (int c = column, r=row, ind = 0; 
+                c < (column+length); 
+                c++, r++, ind++) {
+            gls[ind] = new GridLocation(r,c);
+        }
+        
+        return gls;
+    }
+    
+    private GridLocation[] fill180(int row, int column, int length) {
+        GridLocation[] gls = new GridLocation[length];
+        
+        for (int r = row, ind = 0; r < (row + length); r++, ind++) {
+            gls[ind] = new GridLocation(r,column);
+        }
+        
+        return gls;
+    }
+    
+    private GridLocation[] fill225(int row, int column, int length) {
+        GridLocation[] gls = new GridLocation[length];
+        
+        int c = column;
+        int ind = 0;
+        for (int r = row; r < (row+length); r++) {
+            gls[ind] = new GridLocation(r,c);
+            --c;
+            ++ind;
+        }
+        
+        return gls;
+    }
 }
---
+++ simple/src/main/java/org/allnix/simple/ch3/WordSearchConstraint.java
@@ -0,0 +1,71 @@
+package org.allnix.simple.ch3;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class WordSearchConstraint extends Constraint<String, GridLocation[]>{
+    /**
+     * ./gradlew -PmainClass=org.allnix.simple.ch3.WordSearchConstraint runApp
+     * gradlew.bat -PmainClass=org.allnix.simple.ch3.WordSearchConstraint runApp
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        
+        WordGrid grid = new WordGrid(9, 9);
+        List<String> words = List.of(
+                "MATTHEW", "JOE", "MARY", "SARAH", "SALLY"
+                );
+        Map<String, List<GridLocation[]>> domainDb = new HashMap<>();
+        for (String word : words) {
+            domainDb.put(word, grid.generateDomain(word));
+        }
+        
+        CSP<String,GridLocation[]> csp = new CSP<>(words, domainDb);
+        csp.addConstraint(new WordSearchConstraint(words));
+        Map<String, GridLocation[]> solution = csp.backtrackingSearch();
+        if (solution == null) {
+            out.println("No solution found!");
+        } else {
+            Random random = new Random();
+            for (Entry<String,GridLocation[]> item : solution.entrySet()) {
+                String word = item.getKey();
+                GridLocation[] locations = item.getValue();
+                
+                // Reverse the word
+//                if (random.nextBoolean()) {
+//                    Arrays.reverse
+//                }
+                grid.mark(word, locations);
+            }
+            out.println(grid);
+        }
+    }
+    public WordSearchConstraint(List<String> variables) {
+        super(variables);
+    }
+
+    @Override
+    public boolean satisfied(Map<String, GridLocation[]> assignment) {
+//        Arrays.stream(assignment.values());
+        List<GridLocation> allLocations = assignment
+                .values() // Collection<GridLocation[]>
+                .stream() // GridLocation[], GridLocation[] ...
+                .flatMap(Arrays::stream) // stream(GridLocation[]) + stream(GridLocation[]) ...  
+                .collect(Collectors.toList());
+        
+        Set<GridLocation> allLocationSet = new HashSet<>(allLocations);
+        
+        return allLocations.size() == allLocationSet.size();
+    }
+
+}
